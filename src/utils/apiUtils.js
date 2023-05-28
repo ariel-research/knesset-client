e3e431cc486fbc05eb286f61e1a609c17d259abe
rafasu4 <raf59101@gmail.com>
2023-05-28 12:26:31 +0300

    👽route change

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://politics-backend.onrender.com/general";
 
 export const getAllBills = async () => {
-  return await axios.get(`${BASE_URL}/general/bills`);
+  return await axios.get(`${BASE_URL}/bills`);
 };
