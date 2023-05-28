b730ea8205302b535059ae49ff017583ce332353
rafasu4 <raf59101@gmail.com>
2023-05-28 23:31:51 +0300

    🚧compass page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import styled from 'styled-components';
-import BillsSelectionPage from './pages/BillsSelectionPage';
+import styled from "styled-components";
+import BillsSelectionPage from "./pages/BillsSelectionPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CompassResultsPage from "./pages/CompassResultsPage";
 
 function App() {
-
   return (
-    <AppWrapper>
-      <BillsSelectionPage />
-    </AppWrapper>
+    <Router>
+      <AppWrapper>
+        <Routes>
+          <Route path="/" element={<BillsSelectionPage />} />
+          <Route path="/results" element={<CompassResultsPage />} />
+        </Routes>
+      </AppWrapper>
+    </Router>
   );
 }
 
