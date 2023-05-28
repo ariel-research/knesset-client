571dba1338606489a2cd002de96555b43d9d7470
rafasu4 <raf59101@gmail.com>
2023-05-28 19:28:23 +0300

    👽base url

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://politics-backend.onrender.com/general";
+const BASE_URL = "https://politics-transperancy-backend.onrender.com/general/";
 
 export const getAllBills = async () => {
   return await axios.get(`${BASE_URL}/bills`);
