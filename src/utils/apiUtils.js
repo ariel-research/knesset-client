1b6da2f576b983175515b85d3fb7dc2700d99834
rafasu4 <raf59101@gmail.com>
2023-06-04 21:01:22 +0300

    🐛missing rout

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = "https://politics-transperancy-backend.onrender.com";
+const BASE_URL = "https://politics-transperancy-backend.onrender.com/";
 
 export const getAllBills = async () => {
-  return await axios.get(`${BASE_URL}/bills`);
+  return await axios.get(`${BASE_URL}general/bills`);
 };
 
 export const getAllKnessetNum = async () => {
-  return await axios.get(`${BASE_URL}/general//knessetAmounts`);
+  return await axios.get(`${BASE_URL}general/knessetAmounts`);
 };
 
 export const getBillsOfKnesset = async (knesset_num) => {
   return await axios.get(
-    `${BASE_URL}/general//billsByKnessetNum?knessetNum=${knesset_num}`
+    `${BASE_URL}general//billsByKnessetNum?knessetNum=${knesset_num}`
   );
 };
 
 export const getVotesScore = async (body) => {
-  return await axios.get(`${BASE_URL}/scores`, body);
+  return await axios.get(`${BASE_URL}scores`, body);
 };
