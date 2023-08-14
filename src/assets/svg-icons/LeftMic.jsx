218c09ded3875266b9c7d268bf5d6cd8e3023f85
Stycks4 <raphael.suliman@globekeeper.com>
2023-08-14 14:20:04 +0300

    ♻️general restructure

---
+++ src/assets/svg-icons/LeftMic.jsx
@@ -0,0 +1,8 @@
+const LeftMic = () => (
+    <svg width="20" height="105" viewBox="0 0 23 105" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="15.1406" width="8.26726" height="15.0125" rx="4.13363" transform="rotate(29.7811 15.1406 0)" fill="black" />
+        <path d="M2 104.01V32.9634C2 32.0891 2.22928 31.23 2.66498 30.4719L15 9.00989" stroke="black" stroke-width="4" />
+    </svg>
+);
+
+export default LeftMic;
---
+++ src/assets/svg-icons/RightMic.jsx
@@ -0,0 +1,9 @@
+const RightMic = () => (
+    <svg width="20" height="105" viewBox="0 0 23 105" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="8.26726" height="15.0125" rx="4.13363" transform="matrix(-0.86793 0.496687 0.496687 0.86793 7.85944 0)" fill="black" />
+        <path d="M21 104.01V32.9634C21 32.0891 20.7707 31.23 20.335 30.4719L8 9.00989" stroke="black" stroke-width="4" />
+    </svg>
+
+);
+
+export default RightMic;
